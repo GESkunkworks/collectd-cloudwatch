ce2702cde4fcd194c226d29ca9b6f50b0eb7707d
212726866 <christian.tucker@ge.com>
2018-12-11 13:56:44 -0500

    removing spaces and added # to region

--- src/setup.py
+++ src/setup.py
@@ -381,10 +381,10 @@ class InteractiveConfigurator(object):
     def _configure_dimensions_path_non_interactive(self):
         try:
             with open(DEFAULT_DIMENSIONS_FILE, "w") as dimensions_file:
-                dimensions_file.write('#You can use all of the instance identity documents dimensions\n  \
-                #Make sure that the first letter of the dimension is captilized and there are no spaces at the end of the dimension\n \
-                #For more information on dimensions that can be used: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-identity-documents.html\n\n \
-                #Host\n #PluginInstance\n #AvailabilityZone\n #InstanceId\n Region\n')
+                dimensions_file.write('#You can use all of the instance identity documents dimensions\n\
+                #Make sure that the first letter of the dimension is captilized and there are no spaces at the end of the dimension\n\
+                #For more information on dimensions that can be used: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-identity-documents.html\n\n\
+                #Host\n #PluginInstance\n #AvailabilityZone\n #InstanceId\n #Region\n')
                 print(Color.green("Dimensions File written successfully."))
         except IOError as e:
             raise InstallationFailedException("Could not write dimensions file. Cause: {}".format(str(e)))
