ae0cad5ec61cf22b33c3ce3a370c4785a3b0d312
212726866 <christian.tucker@ge.com>
2018-12-12 11:37:41 -0500

    testing non-interactive setup then switch to dimensions used

--- src/cloudwatch_writer.py
+++ src/cloudwatch_writer.py
@@ -24,6 +24,7 @@ def aws_init():
         config = ConfigHelper()
         _LOGGER.info("Does Dimension Exist?: " + str(config.dimensions[0]))
         flusher = Flusher(config_helper=config,  dataset_resolver=get_dataset_resolver())
+        _LOGGER.info("Registering Write")
         collectd.register_write(aws_write, data = flusher)
         _LOGGER.info('Initialization finished successfully.')
     except Exception as e:
